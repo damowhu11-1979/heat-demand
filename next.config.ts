5fb0dbcc74e75255c3a8d3ed38d50a7f2613b541
damowhu11-1979 <damowhu11@gmail.com>
2025-11-04 16:44:34 +0000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,13 @@
-// next.config.ts  (root)
 import type { NextConfig } from 'next';
 
 const isCI = process.env.GITHUB_ACTIONS === 'true';
 const repo = 'heat-demand'; // change if your repo name differs
 
 const config: NextConfig = {
-  output: 'export',            // Next 16+ static export
+  output: 'export',
   images: { unoptimized: true },
   basePath: isCI ? `/${repo}` : undefined,
-  assetPrefix: isCI ? `/${repo}/` : undefined,
+  assetPrefix: isCI ? `/${repo}/` : undefined
 };
 
 export default config;
