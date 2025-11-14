bd017424eaa3147ed02979a5979149472398531b
damowhu11-1979 <damowhu11@gmail.com>
2025-11-14 15:39:18 +0000

    Update page.tsx

--- app/ventilation/page.tsx
+++ app/ventilation/page.tsx
@@ -15,7 +15,7 @@ export default function ElementsPage(): React.JSX.Element {
       {/* page header */}
       <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: 12 }}>
         <h1 style={{ margin: 0 }}>Building Elements</h1>
-        <ClearDataButton />
+        
       </div>
 
       {/* ...the rest of your existing elements page UI... */}
