22f0f2330621bb8039704bda484bc568b0aadb48
damowhu11-1979 <damowhu11@gmail.com>
2025-11-17 11:09:02 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
  'use client';
-
-import React from 'react';
 import Link from 'next/link';
 import ClearDataButton from '@/components/ClearDataButton';
 import React, { useEffect, useRef, useState } from 'react';
