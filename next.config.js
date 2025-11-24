551fac8377b490760b6397325592ba2b5cc5c863
damowhu11-1979 <damowhu11@gmail.com>
2025-11-24 13:09:44 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   output: 'export',          // static export for GH Pages
-  basePath: '/heat-demand',  // repo name
+  basePath: '/heat-demand',  // your repo name
   assetPrefix: '/heat-demand/',
   trailingSlash: true,       // creates folder-style files
 };
