67ca405947b04944b3c57de283b9619601517450
damowhu11-1979 <damowhu11@gmail.com>
2025-12-05 16:04:14 +0000

    Update RoomDesignTool.jsx

--- src/components/RoomDesignTool.jsx
+++ src/components/RoomDesignTool.jsx
@@ -1,1 +1,12 @@
+import React from 'react';
+// Import the component from its new location
+import RoomDesigner from '../../components/RoomDesigner'; 
 
+function RoomsPage() {
+  return (
+    // This is where you render the component you just added
+    <RoomDesigner />
+  );
+}
+
+export default RoomsPage;
